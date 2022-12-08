4f57efe282646b0048892516532f6e4f9cde4beb
Diana-2604 <Diana8991@yandex.ru>
2022-12-09 00:10:41 +0300

    Seminar1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+// Реализуйте метод, принимающий в качестве аргумента целочисленный массив и некоторое значение.
+// Метод ищет в массиве заданное значение и возвращает его индекс. При этом, например:
+// 1. если длина массива меньше некоторого заданного минимума, метод возвращает -1 в качестве кода ошибки
+// 2. если искомый элемент не найден, метод вернет -2 в качестве кода ошибки.
+// 3. если вместо массива пришел null, метод вернет -3
+// Напишите метод, в котором реализуйте взаимодействие с пользователем. То есть, этот метод запросит искомое
+// число у пользователя, обработает возвращенное значение и покажет читаемый результат пользователю.
+// Например, если вернутся -2, вывести пользователю: "Искомый элемент не найден"
+
+import java.util.Scanner;
 public class Main {
+    private final static int MIN_LENGTH = 5;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = {1, 2, 3, 4, 5, 6}; /* option + command + L to reformat */
+        Scanner iScanner = new Scanner(System.in);
+        System.out.print("Enter search value: "); /* sout to call System.out.println*/
+        int value = iScanner.nextInt();
+        iScanner.close();
+
+        int result = findIndexOf(value, array);
+        if (result == -1) {
+            System.out.println("Заданный массив меньше минимального значения");
+        } else if (result == -2) {
+            System.out.println("Искомый элемент не найден");
+        } else if (result == -3) {
+            System.out.println("Заданный массив не содержит значений для поиска");
+        } else {
+            System.out.println("Индекс искомого элемента: " + result);
+        }
+    }
+
+    private static int findIndexOf(int value, int[] array) {
+        if (array == null) return -3;
+
+        if (array.length < MIN_LENGTH) {
+            return -1;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (value == array[i]) {
+                return i;
+            }
+        }
+        return -2; /* value not found */
     }
 }
---
+++ src/StackTrace.java
@@ -0,0 +1,23 @@
+// Get stack trace
+
+public class StackTrace {
+    public static void main(String[] args) {
+        a();
+    }
+
+    private static void a() {
+        b();
+    }
+
+    private static void b() {
+        c();
+    }
+
+    private static void c() {
+        System.out.println("Calling method c to get stack trace: ");
+        for (var trace : Thread.currentThread().getStackTrace()) {
+            System.out.println(trace);
+        }
+        System.out.println(">> Stack structure LIFO : Last In First Out");
+    }
+}
